ed01127d7408149d61d1980f6cc275bd1fdba982
Xu <ixuuux@qq.com>
2018-10-17 10:41:21 +0800

    Update requests_get.py

--- requests_get.py
+++ requests_get.py
@@ -6,9 +6,9 @@ def get(url='https://httpbin.org/ip', headers=None, proxies=None, try_num=0, try
             response = requests.get(url=url, headers=headers, proxies=proxies, timeout=timeout)
             if response.status_code == 200:
                 return response
-            return get(url, try_num=try_num+1, try_max_num=try_max_num)
+            return get(url=url, try_num=try_num+1, try_max_num=try_max_num)
         except:
-            return get(url, try_num=try_num+1, try_max_num=try_max_num)
+            return get(url=url, try_num=try_num+1, try_max_num=try_max_num)
     return False
 
 

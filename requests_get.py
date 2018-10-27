c53bae4c31c6d3478283bdc016260668421216b1
Xu <ixuuux@qq.com>
2018-10-28 00:21:56 +0800

    Update requests_get.py

--- requests_get.py
+++ requests_get.py
@@ -8,7 +8,7 @@ def get(url='https://httpbin.org/ip', headers=None, proxies=None, try_num=0, try
             head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
                              '(KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
         try:
-            response = requests.get(url=url, headers=headers, proxies=proxies, timeout=timeout)
+            response = requests.get(url=url, headers=head, proxies=proxies, timeout=timeout)
             if response.status_code == 200:
                 return response
             return get(url=url, headers=head, proxies=proxies, try_num=try_num+1, try_max_num=try_max_num, timeout=timeout)
